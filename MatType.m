cb362d1de16427fe863b7deaedaee7194b1b04dd
Bastian Bechtold <bastian.bechtold@jade-hs.de>
2018-03-05 11:13:15 +0100

    a bit of cleanup of code and figure

--- MatType.m
+++ MatType.m
@@ -3,78 +3,84 @@ classdef MatType < handle
         Figure
         TemplateCharacters
         TypingCharacters
-        xMargin = 10
-        yMargin = 10
-        charWidth = 7
-        charHeight = 12
-        cursorIdx
+        CursorIdx
+        XMargin = 10
+        YMargin = 10
+        CharWidth = 7
+        CharHeight = 12
+
     end
     methods
         function obj = MatType(defaultText)
+            obj.Figure = figure();
+            obj.Figure.MenuBar = 'none';
+            obj.Figure.ToolBar = 'none';
+            obj.Figure.Resize = 'off';
+            obj.Figure.Name = 'MatType';
+            obj.Figure.NumberTitle = 'off';
+            obj.Figure.KeyPressFcn = @obj.KeyPress;
+
             if ~exist('defaultText') || isempty(defaultText)
                 templateText = 'lorem ipsum dolor sit amet';
             else
                 templateText = defaultText;
             end
-
-            obj.Figure = figure();
             obj.CreateCharacters(templateText);
-            obj.cursorIdx = 1;
 
-            obj.Figure.KeyPressFcn = @obj.KeyPress;
-
+            obj.CursorIdx = 1;
         end
+
         function KeyPress(obj, handle, event)
             c = event.Character;
-            if obj.cursorIdx > length(obj.TemplateCharacters)
+            if obj.CursorIdx > length(obj.TemplateCharacters)
                 return
             elseif c >= ' ' && c <= '~'
-                typingCharacter = obj.TypingCharacters(obj.cursorIdx);
+                typingCharacter = obj.TypingCharacters(obj.CursorIdx);
                 typingCharacter.String = c;
-                templateCharacter = obj.TemplateCharacters(obj.cursorIdx);
+                templateCharacter = obj.TemplateCharacters(obj.CursorIdx);
                 if templateCharacter.String == typingCharacter.String
                     templateCharacter.ForegroundColor = [0 1 0];
                 else
                     templateCharacter.ForegroundColor = [1 0 0];
                 end
-                obj.cursorIdx = obj.cursorIdx + 1;
-            elseif strcmp(event.Key, 'backspace') && obj.cursorIdx > 1
-                obj.cursorIdx = obj.cursorIdx - 1;
-                templateCharacter = obj.TemplateCharacters(obj.cursorIdx);
+                obj.CursorIdx = obj.CursorIdx + 1;
+            elseif strcmp(event.Key, 'backspace') && obj.CursorIdx > 1
+                obj.CursorIdx = obj.CursorIdx - 1;
+                templateCharacter = obj.TemplateCharacters(obj.CursorIdx);
                 templateCharacter.ForegroundColor = [0 0 0];
-                typingCharacter = obj.TypingCharacters(obj.cursorIdx);
+                typingCharacter = obj.TypingCharacters(obj.CursorIdx);
                 typingCharacter.String = ' ';
             end
         end
 
         function CreateCharacters(obj, text)
             figSize = obj.Figure.Position(3:4);
-            xPos = obj.yMargin;
-            yPos = figSize(2) - obj.yMargin - obj.charHeight;
+            xPos = obj.XMargin;
+            yPos = figSize(2) - obj.YMargin - obj.CharHeight;
             for idx=1:length(text)
                 character = uicontrol('style', 'text');
                 character.Position = ...
-                    [xPos, yPos, obj.charWidth, obj.charHeight+2];
+                    [xPos, yPos, obj.CharWidth, obj.CharHeight+2];
                 character.FontName = 'FixedWidth';
                 character.FontUnits = 'pixels';
-                character.FontSize = obj.charHeight;
+                character.FontSize = obj.CharHeight;
                 character.String = text(idx);
                 obj.TemplateCharacters = [obj.TemplateCharacters character];
 
                 character = uicontrol('style', 'text');
                 character.Position = ...
-                    [xPos, yPos-figSize(2)/2, obj.charWidth, obj.charHeight+2];
+                    [xPos, yPos-figSize(2)/2, obj.CharWidth, obj.CharHeight+2];
                 character.FontName = 'FixedWidth';
                 character.FontUnits = 'pixels';
-                character.FontSize = obj.charHeight;
+                character.FontSize = obj.CharHeight;
                 character.String = 'X';
                 obj.TypingCharacters = [obj.TypingCharacters character];
 
-                if (xPos + obj.charWidth + 1) > (figSize(1) - obj.xMargin)
-                    xPos = obj.xMargin;
-                    yPos = yPos - obj.charHeight + 2;
+                if (xPos + obj.CharWidth + 1) > (figSize(1) - obj.XMargin)
+                    xPos = obj.XMargin;
+                    yPos = yPos - obj.CharHeight + 2;
                 else
-                    xPos = xPos + obj.charWidth + 1;
+                    xPos = xPos + obj.CharWidth + 1;
                 end
             end
         end
