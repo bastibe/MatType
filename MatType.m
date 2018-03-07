f378532b5d5be0002a153b62831c33598b9805f5
Bastian Bechtold <bastian.bechtold@jade-hs.de>
2018-03-07 13:24:15 +0100

    adds license

--- MatType.m
+++ MatType.m
@@ -7,6 +7,21 @@ classdef MatType < handle
     %
     % You can supply your own text with MatType(text).
 
+    % Copyright (C) 2018 Bastian Bechtold
+    %
+    % This program is free software: you can redistribute it and/or
+    % modify it under the terms of the GNU General Public License as
+    % published by the Free Software Foundation, either version 3 of
+    % the License, or (at your option) any later version.
+    %
+    % This program is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    % GNU General Public License for more details.
+    %
+    % For the full text of the GNU General Public License, see
+    % <https://www.gnu.org/licenses/>.
+
     properties
         % UI components:
         Figure
