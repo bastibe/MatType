1c330d280be623db1ce94f0c0446e31ebe3ee0fa
Bastian <basti@bastibe.de>
2018-03-09 13:54:06 +0100

    fixes non-ASCII characters on non-UTF-8 Matlabs

--- MatType.m
+++ MatType.m
@@ -341,7 +341,7 @@ classdef MatType < handle
         function text = DefaultText(obj)
             % A default text (in German)
             text = 'Einst stritten sich Nordwind und Sonne, wer von ihnen beiden wohl der Stärkere wäre, als ein Wanderer, der in einen warmen Mantel gehüllt war, des Weges daherkam. Sie wurden einig, daß derjenige für den Stärkeren gelten sollte, der den Wanderer zwingen würde, seinen Mantel abzunehmen. Der Nordwind blies mit aller Macht, aber je mehr er blies, desto fester hüllte sich der Wanderer in seinen Mantel ein. Endlich gab der Nordwind den Kampf auf. Nun erwärmte die Sonne die Luft mit ihren freundlichen Strahlen, und schon nach wenigen Augenblicken zog der Wanderer seinen Mantel aus. Da mußte der Nordwind zugeben, daß die Sonne von ihnen beiden der Stärkere war.';
-            text = native2unicode(text, 'UTF-8');
+            text = native2unicode(unicode2native(text), 'UTF-8');
         end
     end
 end
